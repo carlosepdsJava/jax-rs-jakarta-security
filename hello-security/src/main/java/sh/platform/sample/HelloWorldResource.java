a09f9472f71275fe14663ed656cb5e57a78835f5
Carlos Santos <carlossantos@MacBook-Pro.local>
2020-06-21 14:54:33 -0300

    updated microprofile version and bean-discovery-mode to all

--- hello-security/src/main/java/sh/platform/sample/HelloWorldResource.java
+++ hello-security/src/main/java/sh/platform/sample/HelloWorldResource.java
@@ -8,7 +8,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("")
+@Path("/")
 @RequestScoped
 public class HelloWorldResource {
 
--- oauth2-jwt-mongodb/src/main/java/sh/platform/sample/security/UserRepository.java
+++ oauth2-jwt-mongodb/src/main/java/sh/platform/sample/security/UserRepository.java
@@ -2,6 +2,7 @@ package sh.platform.sample.security;
 
 import jakarta.nosql.mapping.Repository;
 
+import javax.decorator.Decorator;
 import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 import java.util.Optional;
